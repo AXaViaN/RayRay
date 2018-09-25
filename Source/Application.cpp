2a33f95fcd0ab26f8492f4d7480e8dd0af517ed5
Ahmet Selim CEVIK <sasdwsas@gmail.com>
2018-09-26 02:32:54 +0300

    Add a random scene

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -8,6 +8,7 @@
 #include <Tool/Texture.h>
 #include <Utility/Color.h>
 #include <Utility/Vector2.h>
+#include <Utility/Random.h>
 #include <string>
 
 static const std::string OutputFileName = "output";
@@ -17,6 +18,7 @@ static constexpr size_t AASampleCount = 128;
 static Entity::Scene CreateScene()
 {
 	Entity::Scene scene;
+	Utility::Random random;
 
 	Utility::Vector3f position;
 	float radius;
@@ -25,48 +27,95 @@ static Entity::Scene CreateScene()
 	Entity::DielectricMaterial dielectricMaterial(0);
 
 	// Ground
-	position = {0.0f, -100.5f, -1.0f};
-	radius = 100.0f;
-	lambertianMaterial = Entity::LambertianMaterial(Utility::Color{0.8f, 0.8f, 0.0f});
+	position = {0.0f, -1000.0f, 0.0f};
+	radius = 1000.0f;
+	lambertianMaterial = Entity::LambertianMaterial(Utility::Color{0.75f, 0.75f, 0.75f});
 	scene.AddSceneObject(Entity::Sphere(position, radius, 
 										std::make_unique<Entity::LambertianMaterial>(lambertianMaterial)));
 
 	// Center
-	position = {0.0f, 0.0f, -1.0f};
-	radius = 0.5f;
-	lambertianMaterial = Entity::LambertianMaterial(Utility::Color{0.1f, 0.2f, 0.5f});
+	position = {0.0f, 1.0f, 0.0f};
+	radius = 1.0f;
+	dielectricMaterial = Entity::DielectricMaterial(1.5f);
 	scene.AddSceneObject(Entity::Sphere(position, radius, 
-										std::make_unique<Entity::LambertianMaterial>(lambertianMaterial)));
+										std::make_unique<Entity::DielectricMaterial>(dielectricMaterial)));
 
 	// Right metal
-	position = {1.0f, 0.0f, -1.0f};
-	radius = 0.5f;
-	metalMaterial = Entity::MetalMaterial(Utility::Color{0.8f, 0.6f, 0.2f}, 0.25f);
+	position = {3.0f, 1.0f, 0.0f};
+	radius = 1.0f;
+	metalMaterial = Entity::MetalMaterial(Utility::Color{0.7f, 0.6f, 0.5f}, 0.0f);
 	scene.AddSceneObject(Entity::Sphere(position, radius, 
 										std::make_unique<Entity::MetalMaterial>(metalMaterial)));
 
 	// Left glass
-	position = {-1.0f, 0.0f, -1.0f};
-	radius = 0.5f;
+	position = {-3.0f, 1.0f, 0.0f};
+	radius = 1.0f;
 	dielectricMaterial = Entity::DielectricMaterial(1.5f);
 	scene.AddSceneObject(Entity::Sphere(position, radius, 
 										std::make_unique<Entity::DielectricMaterial>(dielectricMaterial)));
-	position = {-1.0f, 0.0f, -1.0f};
-	radius = -0.45f;
+	position = {-3.0f, 1.0f, 0.0f};
+	radius = -0.95f;
 	dielectricMaterial = Entity::DielectricMaterial(1.5f);
 	scene.AddSceneObject(Entity::Sphere(position, radius, 
 										std::make_unique<Entity::DielectricMaterial>(dielectricMaterial)));
+
+	// Left lambertian
+	position = {-6.0f, 1.0f, 0.0f};
+	radius = 1.0f;
+	lambertianMaterial = Entity::LambertianMaterial(Utility::Color{0.2f, 0.4f, 0.8f});
+	scene.AddSceneObject(Entity::Sphere(position, radius, 
+										std::make_unique<Entity::LambertianMaterial>(lambertianMaterial)));
+
+	// Random balls
+#if 1
+	radius = 0.2f;
+	for( int x=-18 ; x<11 ; ++x )
+	{
+		for( int z=-18 ; z<11 ; ++z )
+		{
+			position = {x+0.9f*random.GetFloat(), radius, z+0.9f*random.GetFloat()};
+			if((position-Utility::Vector3f(4.0f, radius, 0.0f)).Length() > 0.9f)
+			{
+				float chooseMaterial = random.GetFloat();
+				if(chooseMaterial < 0.6f)
+				{
+					Utility::Color color{random.GetFloat()*random.GetFloat(), 
+										 random.GetFloat()*random.GetFloat(), 
+										 random.GetFloat()*random.GetFloat()};
+					lambertianMaterial = Entity::LambertianMaterial(color);
+					scene.AddSceneObject(Entity::Sphere(position, radius, 
+														std::make_unique<Entity::LambertianMaterial>(lambertianMaterial)));
+				}
+				else if(chooseMaterial < 0.85f)
+				{
+					Utility::Color color{random.GetFloat()*random.GetFloat(), 
+										 random.GetFloat()*random.GetFloat(), 
+										 random.GetFloat()*random.GetFloat()};
+					metalMaterial = Entity::MetalMaterial(color, 0.5f*random.GetFloat());
+					scene.AddSceneObject(Entity::Sphere(position, radius, 
+														std::make_unique<Entity::MetalMaterial>(metalMaterial)));
+				}
+				else
+				{
+					dielectricMaterial = Entity::DielectricMaterial(1.5f);
+					scene.AddSceneObject(Entity::Sphere(position, radius, 
+														std::make_unique<Entity::DielectricMaterial>(dielectricMaterial)));
+				}
+			}
+		}
+	}
+#endif
 
 	return scene;
 }
 
 static Entity::Camera SetupCamera()
 {
-	Utility::Vector3f position = {3.0f, 3.0f, 2.0f};
-	Utility::Vector3f lookat = {0.0f, 0.0f, -1.0f};
+	Utility::Vector3f position = {9.0f, 2.0f, 5.0f};
+	Utility::Vector3f lookat = {0.0f, 0.0f, 0.0f};
 	Utility::Vector3f up = {0.0f, 1.0f, 0.0f};
-	float fov = 20.0f;
-	float aperture = 2.0f;
+	float fov = 30.0f;
+	float aperture = 0.1f;
 	float aspectRatio = OutputSize.X / (float)OutputSize.Y;
 
 	return Entity::Camera(position, lookat, up, fov, aperture, aspectRatio);
@@ -84,7 +133,7 @@ int main()
 	Entity::Renderer renderer(OutputSize, AASampleCount);
 	Tool::Texture renderTexture = renderer.RenderScene(scene, camera);
 
-	renderTexture.Save(OutputFileName);
+	renderTexture.Save(OutputFileName, 2.2f);
 
 	return 0;
 }
